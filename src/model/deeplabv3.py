46f7c49f5aab0b113c7dfe3ff727463cf1ebe3fc
renesat <smol.ivan97@gmail.com>
2021-10-29 23:38:49 +0300

    Fix device

--- src/model/deeplabv3.py
+++ src/model/deeplabv3.py
@@ -58,14 +58,16 @@ class TrashDetector(pl.LightningModule):
 
         if batch_idx == 0:
             img = (
-                x * torch.Tensor([0.229, 0.224, 0.225]).resize(1, 3, 1, 1)
+                x * torch.Tensor([0.229, 0.224, 0.225]).to(x.device).resize(1, 3, 1, 1)
                 + torch.Tensor(
                     [
                         0.485,
                         0.456,
                         0.406,
                     ]
-                ).resize(1, 3, 1, 1)
+                )
+                .to(x.device)
+                .resize(1, 3, 1, 1)
             )
             img[img > 1] = 1
             img[img < 0] = 0
